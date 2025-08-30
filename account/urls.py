3d30c4160f7746d794b241444ba06afc338c301e
7Learn <7Learn@gmail.com>
2025-08-30 15:11:41 +0900

    feat(auth): add SMS-based registration with OTP verification

--- account/urls.py
+++ account/urls.py
@@ -3,6 +3,8 @@ from . import views
 
 app_name = 'account'
 urlpatterns=[
-    path('login', views.UserLogin.as_view(), name='user_login')
+    path('login', views.UserLogin.as_view(), name='user_login'),
+    path('otplogin', views.OtpLoginView.as_view(), name='user_otp_login'),
+    path('otp', views.CheckOtpView.as_view(), name='check_otp')
 ]
 
