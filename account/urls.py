9168885f9bd8f0f90ecb5183c75a9fa397e15dae
7Learn <7Learn@gmail.com>
2025-09-01 16:56:45 +0900

    feat(auth): add logout view and fix redirect after login/logout

--- account/urls.py
+++ account/urls.py
@@ -5,6 +5,7 @@ app_name = 'account'
 urlpatterns=[
     path('login', views.UserLogin.as_view(), name='user_login'),
     path('otplogin', views.OtpLoginView.as_view(), name='user_otp_login'),
-    path('otp', views.CheckOtpView.as_view(), name='check_otp')
+    path('otp', views.CheckOtpView.as_view(), name='check_otp'),
+    path('lohout', views.user_logout, name='user_logout')
 ]
 
