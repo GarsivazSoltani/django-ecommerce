5ade9232a4c6f60cf7a341fa9d0c17f3b16d1a97
Garsivaz Soltani <garsi.soltani@gmail.com>
2025-09-08 15:30:37 +0900

    feat(products): add shopping cart

--- home/views.py
+++ home/views.py
@@ -3,3 +3,12 @@ from django.views.generic import TemplateView
 
 class Home(TemplateView):
     template_name = "home/index.html"
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+
+        # self.request.session['my_name'] = 'Garsivaz'
+        self.request.session.get('my_name', 'Garsivaz')
+        # print(self.request.session['my_name'])
+        # del self.request.session['my_name']
+        return context
--- multi_shop/urls.py
+++ multi_shop/urls.py
@@ -7,5 +7,6 @@ urlpatterns = [
     path('', include('home.urls')),
     path('admin/', admin.site.urls),
     path('account/', include('account.urls')),
-    path('products/', include('product.urls'))
+    path('products/', include('product.urls')),
+    path('cart/', include('cart.urls'))
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
